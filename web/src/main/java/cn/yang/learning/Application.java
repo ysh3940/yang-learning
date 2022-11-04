9d00a4d73ebaef91d89170396b92d7e3938a2723
yangshenhui <yangshenhui@jd.com>
2022-11-04 22:33:34 +0800

    1

--- web/src/main/java/cn/yang/learning/Application.java
+++ web/src/main/java/cn/yang/learning/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Application {
 
     public static void main(String[] args) {
         // --spring.profiles.active=dev
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
