df623b6b18f0f61c715e8828bdb2c36ea5cdf12d
yangshenhui <yangshenhui@jd.com>
2022-11-05 22:55:26 +0800

    update grpc demo

--- grpc-proto-client/src/main/java/cn/yang/learning/service/GrpcClientMyService.java
+++ grpc-proto-client/src/main/java/cn/yang/learning/service/GrpcClientMyService.java
@@ -2,7 +2,8 @@ package cn.yang.learning.service;
 
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
-import net.devh.boot.grpc.examples.lib.HelloWorldProto;
+import net.devh.boot.grpc.examples.lib.HelloReply;
+import net.devh.boot.grpc.examples.lib.HelloRequest;
 import net.devh.boot.grpc.examples.lib.MyServiceGrpc;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class GrpcClientMyService {
 
     public String sendMessage(final String name) {
         try {
-            final HelloWorldProto.HelloReply response = this.myServiceBlockingStub.sayHello(HelloWorldProto.HelloRequest.newBuilder().setName(name).build());
+            final HelloReply response = this.myServiceBlockingStub.sayHello(HelloRequest.newBuilder().setName(name).build());
             return response.getMessage();
         } catch (Exception e) {
             log.error("Request failed", e);
--- grpc-proto-server/src/main/java/cn/yang/learning/service/GrpcServerMyService.java
+++ grpc-proto-server/src/main/java/cn/yang/learning/service/GrpcServerMyService.java
@@ -2,7 +2,8 @@ package cn.yang.learning.service;
 
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.examples.lib.HelloWorldProto;
+import net.devh.boot.grpc.examples.lib.HelloReply;
+import net.devh.boot.grpc.examples.lib.HelloRequest;
 import net.devh.boot.grpc.examples.lib.MyServiceGrpc;
 import net.devh.boot.grpc.server.service.GrpcService;
 
@@ -11,10 +12,10 @@ import net.devh.boot.grpc.server.service.GrpcService;
 public class GrpcServerMyService extends MyServiceGrpc.MyServiceImplBase {
 
     @Override
-    public void sayHello(HelloWorldProto.HelloRequest req, StreamObserver<HelloWorldProto.HelloReply> responseObserver) {
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
         log.error("GrpcServerMyService.req = {}", req.getName());
 
-        HelloWorldProto.HelloReply reply = HelloWorldProto.HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
     }
