874395ebf9dc2c27d9bedd6612097bc56d2ff163
yangshenhui <yangshenhui@jd.com>
2022-11-17 21:25:57 +0800

    update param request

--- web/src/main/java/cn/yang/learning/interceptor/ParamFilter.java
+++ web/src/main/java/cn/yang/learning/interceptor/ParamFilter.java
@@ -1,12 +1,16 @@
 package cn.yang.learning.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.UUID;
 
 @Slf4j
 @WebFilter(filterName = "paramFilter", urlPatterns = "/*")
@@ -22,7 +26,20 @@ public class ParamFilter implements Filter {
             log.warn("ParamHttpServletRequestWrapper error = {}", e.getMessage(), e);
         }
 
-        chain.doFilter((Objects.isNull(paramHttpServletRequestWrapper) ? request : paramHttpServletRequestWrapper), response);
+        ParamHttpServletResponseWrapper responseWrapper = new ParamHttpServletResponseWrapper((HttpServletResponse) response);
+
+        chain.doFilter((Objects.isNull(paramHttpServletRequestWrapper) ? request : paramHttpServletRequestWrapper), responseWrapper);
+
+
+        String respBody = responseWrapper.getBodyString();
+        JSONObject respJson = JSON.parseObject(respBody);
+        respJson.put("responseId", UUID.randomUUID().toString());
+        System.err.println(respJson.toJSONString());
+
+        response.setContentLength(respJson.toJSONString().length());
+        response.setContentType("application/json;charset=utf-8");
+        response.getOutputStream().write(respJson.toJSONString().getBytes());
+
     }
 
 }
---
+++ web/src/main/java/cn/yang/learning/interceptor/ParamHttpServletResponseWrapper.java
@@ -0,0 +1,87 @@
+package cn.yang.learning.interceptor;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+
+public class ParamHttpServletResponseWrapper extends HttpServletResponseWrapper {
+
+    private final WrapperServletOutputStream wrapperServletOutputStream = new WrapperServletOutputStream();
+
+    public ParamHttpServletResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return wrapperServletOutputStream;
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return new PrintWriter(wrapperServletOutputStream);
+    }
+
+
+    public byte[] getBodyBytes() {
+        return wrapperServletOutputStream.out.toByteArray();
+    }
+
+
+    public String getBodyString() {
+        try {
+            return wrapperServletOutputStream.out.toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return "[UNSUPPORTED ENCODING]";
+        }
+    }
+
+
+    public void copyToResponse() {
+        try {
+            getResponse().getOutputStream().write(getBodyBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private class WrapperServletOutputStream extends ServletOutputStream {
+        private ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setWriteListener(WriteListener writeListener) {
+
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            out.write(b);
+        }
+
+
+        @Override
+        public void write(byte[] b) throws IOException {
+            out.write(b);
+        }
+
+
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
+            out.write(b, off, len);
+        }
+    }
+
+}
