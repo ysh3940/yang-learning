42d0e1e732abfbd35a715622285f6f299f13b40c
yangshenhui <yangshenhui@jd.com>
2022-11-22 20:21:14 +0800

    update param request

--- web/src/main/java/cn/yang/learning/autoconfigure/WebMvcConfiguration.java
+++ web/src/main/java/cn/yang/learning/autoconfigure/WebMvcConfiguration.java
@@ -1,8 +1,6 @@
 package cn.yang.learning.autoconfigure;
 
-import cn.yang.learning.interceptor.ParamConvertInterceptor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,12 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    @Autowired
-    ParamConvertInterceptor paramConvertInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(paramConvertInterceptor);
+
     }
 
 
--- web/src/main/java/cn/yang/learning/interceptor/ParamFilter.java
+++ web/src/main/java/cn/yang/learning/interceptor/ParamFilter.java
@@ -8,38 +8,69 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
-@WebFilter(filterName = "paramFilter", urlPatterns = "/*")
+@WebFilter(filterName = "paramFilter", urlPatterns = "/param/*")
 public class ParamFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        ParamHttpServletRequestWrapper paramHttpServletRequestWrapper = null;
-        try {
-            HttpServletRequest req = (HttpServletRequest) request;
-            paramHttpServletRequestWrapper = new ParamHttpServletRequestWrapper(req);
-        } catch (Exception e) {
-            log.warn("ParamHttpServletRequestWrapper error = {}", e.getMessage(), e);
-        }
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
 
-        ParamHttpServletResponseWrapper responseWrapper = new ParamHttpServletResponseWrapper((HttpServletResponse) response);
+        ParamHttpServletRequestWrapper requestWrapper = new ParamHttpServletRequestWrapper(req);
+        ParamHttpServletResponseWrapper responseWrapper = new ParamHttpServletResponseWrapper(resp);
 
-        chain.doFilter((Objects.isNull(paramHttpServletRequestWrapper) ? request : paramHttpServletRequestWrapper), responseWrapper);
+        String uri = req.getRequestURI();
+
+        JSONObject reqBody = getBody(requestWrapper);
+        reqBody.put("requestId", UUID.randomUUID().toString());
+        requestWrapper.setBody(reqBody.toJSONString());
+
+        String okOld = request.getParameter("ok");
+        String okNew = okOld + "-" + UUID.randomUUID().toString();
+
+        Map<String, String[]> map = new HashMap<>(requestWrapper.getParameterMap());
+        map.put("ok", new String[]{okNew});
+        requestWrapper.setParamMap(map);
+
+
+        String api = uri.substring("/param".length());
+        request.getRequestDispatcher(api).forward(requestWrapper, responseWrapper);
 
 
         String respBody = responseWrapper.getBodyString();
+        System.err.println(respBody);
+
         JSONObject respJson = JSON.parseObject(respBody);
         respJson.put("responseId", UUID.randomUUID().toString());
-        System.err.println(respJson.toJSONString());
 
-//        response.setContentLength(respBody.length());
         response.setContentType("application/json;charset=utf-8");
         response.getOutputStream().write(respJson.toJSONString().getBytes());
 
+    }
+
+    public JSONObject getBody(HttpServletRequest request) {
+        StringBuffer body = new StringBuffer();
+        String line = null;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null) {
+                body.append(line);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        if (body.length() > 0) {
+            return JSON.parseObject(body.toString());
+        }
+
+        return null;
     }
 
 }
