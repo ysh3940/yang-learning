3ede425293bfdb5c6b1f5b666851105392250b66
yangshenhui <yangshenhui@jd.com>
2025-03-16 19:51:37 +0800

    update 算法

---
+++ my-notes/src/main/java/cn/yang/learning/notes/note2025项目/redis笔记.java
@@ -0,0 +1,10 @@
+package cn.yang.learning.notes.note2025项目;
+
+public class redis笔记 {
+
+    // 1.Redis为什么单线程却能高效处理高并发？详细解答下，配合图文
+    // 2.Redis Cluster 的分片数 是固定16438吗
+    // 3.redis 同一个时间点过期大量数据会怎么样
+
+
+}
